5c95b506ebd2a9c9f37600671879e7099cc1d57f
Дмитрий <Dmitriifrs@gmail.com>
2024-04-30 09:46:54 +0500

    topSAles section

--- src/Components/Homepage/Topsales/TopSales.tsx
+++ src/Components/Homepage/Topsales/TopSales.tsx
@@ -17,34 +17,41 @@ function TopSales() {
       <section className={styles.topsales}>
          <div className="container">
             <h2 className={styles.topsales__title}>Хиты продаж</h2>
-            <Swiper
-               className={styles.topsales__grid}
-               slidesPerView={4}
-               spaceBetween={20}
-               navigation={{
-                  nextEl: `.${styles.topsales__nextSlide}`,
-                  prevEl: `.${styles.topsales__prevSlide}`,
-               }}
-               modules={[Navigation]}
-            >
+            <div className={styles.topsales__swiperContainer}>
                <button className={styles.topsales__nextSlide}>
                   <MdNavigateNext />
                </button>
                <button className={styles.topsales__prevSlide}>
                   <MdNavigateNext />
                </button>
-               {hits.map((el) => {
-                  return (
-                     <SwiperSlide key={el.id} className={styles.topsales__item}>
-                        <div className={styles.topsales__img}>
-                           <Image src={el.img} alt={el.title} fill style={{ objectFit: "contain" }} />
-                        </div>
-                        <div className={styles.topsales__name}>{el.title}</div>
-                        <div className={styles.topsales__price}>{el.price}</div>
-                     </SwiperSlide>
-                  );
-               })}
-            </Swiper>
+               <Swiper
+                  className={styles.topsales__grid}
+                  slidesPerView={4}
+                  spaceBetween={20}
+                  navigation={{
+                     nextEl: `.${styles.topsales__nextSlide}`,
+                     prevEl: `.${styles.topsales__prevSlide}`,
+                  }}
+                  modules={[Navigation]}
+               >
+                  {hits.map((el) => {
+                     return (
+                        <SwiperSlide key={el.id} className={styles.topsales__item}>
+                           <div className={styles.topsales__imgBody}>
+                              <div className={styles.topsales__img}>
+                                 <Image src={el.img} alt={el.title} fill style={{ objectFit: "contain" }} />
+                              </div>
+                           </div>
+                           <div className={styles.topsales__name}>{el.title}</div>
+                           <div className={styles.topsales__price}>
+                              <div>{el.price}</div>
+                              <button>Подробнее</button>
+                           </div>
+                        </SwiperSlide>
+                     );
+                  })}
+               </Swiper>
+            </div>
          </div>
       </section>
    );
