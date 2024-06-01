da4b371a264d29ddf82440bc00f14353aa6bddcb
Дмитрий <Dmitriifrs@gmail.com>
2024-06-01 13:30:47 +0500

    cart

--- src/Components/Cart/Body.tsx
+++ src/Components/Cart/Body.tsx
@@ -7,27 +7,25 @@ import { FiMinus } from "react-icons/fi";
 function Body() {
    return (
       <div className={styles.body}>
-         <div className={styles.body__top}>
-            <div className={styles.body__img}>
-               <Image src={img} alt="*" fill style={{ objectFit: "contain" }} />
-            </div>
-            <div className={styles.body__title}>
-               <div className={styles.body__title__main}>Инверторный кондиционер Alba, Модель Alba07 7000bTu</div>
-               <div className={styles.body__title__brand}>Бренд: Midea</div>
-            </div>
-            <div className={styles.body__count}>
-               <button className={styles.body__btns}>
-                  <FiMinus />
-               </button>
-               <input type="number" disabled value={1} className={styles.body__countInput} />
-               <button className={styles.body__btns}>
-                  <FaPlus />
-               </button>
-            </div>
-            <div className={styles.body__price}>677 000 сум</div>
+         <div className={styles.body__img}>
+            <Image src={img} alt="*" fill style={{ objectFit: "contain" }} />
          </div>
-         <div className={styles.body__bottom}>
-            <button>Удалить</button>
+         <div className={styles.body__title}>
+            <div className={styles.body__title__main}>Инверторный кондиционер Alba, Модель Alba07 7000bTu</div>
+            <div className={styles.body__title__brand}>Бренд: Midea</div>
+         </div>
+         <div className={styles.body__count}>
+            <button className={styles.body__btns}>
+               <FiMinus />
+            </button>
+            <input type="number" disabled value={1} className={styles.body__countInput} />
+            <button className={styles.body__btns}>
+               <FaPlus />
+            </button>
+         </div>
+         <div className={styles.body__price}>677 000 сум</div>
+         <div className={styles.body__utils}>
+            <button className={styles.body__utils__delete}>Удалить</button>
          </div>
       </div>
    );
--- src/Components/Cart/Order.tsx
+++ src/Components/Cart/Order.tsx
@@ -1,6 +1,19 @@
 import styles from "./Cart.module.scss";
 
 function Order() {
-   return <div className={styles.order}></div>;
+   return (
+      <div className={styles.order}>
+         <div className={styles.order__total}>
+            <div>Сумма заказа</div>
+            <span>25 620 000 сум</span>
+         </div>
+         <div className={styles.order__btn}>
+            <button>Оформить заказ</button>
+         </div>
+         <div className={styles.order__clear}>
+            <button>Очистить корзину</button>
+         </div>
+      </div>
+   );
 }
 export default Order;
