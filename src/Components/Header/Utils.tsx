b610be2fee595ae77add20cf7a58ccc2df668daa
Дмитрий <Dmitriifrs@gmail.com>
2024-06-05 22:42:29 +0500

    fix

--- src/Components/Header/Utils.tsx
+++ src/Components/Header/Utils.tsx
@@ -1,5 +1,4 @@
 import styles from "./Header.module.scss";
-import { FiShoppingCart } from "react-icons/fi";
 import { GrFavorite } from "react-icons/gr";
 import Link from "next/link";
 import CartIcon from "./CartIcon";
@@ -7,7 +6,7 @@ import CartIcon from "./CartIcon";
 function Utils() {
    return (
       <div className={styles.header__utils}>
-         <Link href={"/"} className={styles.header__utils__item}>
+         <Link href={"/wishlist"} className={styles.header__utils__item}>
             <GrFavorite className={styles.header__utils__icon} />
             <span>Избранное</span>
          </Link>
