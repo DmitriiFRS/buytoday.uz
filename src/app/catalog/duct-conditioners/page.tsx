4f4a04bd232718fe497ff094e43cbf1a6b98c181
Дмитрий <Dmitriifrs@gmail.com>
2024-06-10 23:34:28 +0500

    fix

--- src/app/catalog/duct-conditioners/page.tsx
+++ src/app/catalog/duct-conditioners/page.tsx
@@ -2,41 +2,8 @@ import fetchGraphql from "@/Functions/fetchGraphql";
 import styles from "@/Components/Aircond&SemiInd/AircondSemi.module.scss";
 import NextBreadcrumb from "@/Components/Utilities/Breadcrumbs";
 import Grid from "@/Components/SemiIndConditioners/Grid";
-
-type SemiIndImgCollection = {
-   url: string;
-};
-
-type SemiIndModelCollection = {
-   price: number;
-   model: string;
-};
-
-export type SemiIndDataInner = {
-   name: string;
-   url: string;
-   company: string;
-   temperatureRange: string;
-   isInverter: boolean;
-   type: string;
-   imageCollection: {
-      items: SemiIndImgCollection[];
-   };
-   semiCondModelCollection: {
-      items: SemiIndModelCollection[];
-   };
-};
-
-export type Data = {
-   data: {
-      dollarValue: {
-         value: number;
-      };
-      semiIndustrialCollection: {
-         items: SemiIndDataInner[];
-      };
-   };
-};
+import GridContainer from "@/Components/SemiIndConditioners/GridContainer";
+import { Data } from "../col-conditioners/page";
 
 const title = "Канальные сплит-системы";
 const type = "Канальный";
@@ -44,38 +11,39 @@ const uri = "duct-conditioners";
 
 async function page() {
    const data: Data = await fetchGraphql(`
-   query {
-      dollarValue(id: "1tU030J3VGI8BlTOgn7Sjk") {
-         value
-      }
-      semiIndustrialCollection(limit: 50) {
-        items {
-          name
-          url
-          company
-          temperatureRange
-          isInverter
-          type
-          imageCollection(limit: 99) {
-            items {
-              url
-            }
-          }
-          semiCondModelCollection(limit: 99) {
-            items {
-              price
-              model
-            }
-          }
-        }
-      }
-    }
-   `);
+      query {
+         dollarValue(id: "1tU030J3VGI8BlTOgn7Sjk") {
+            value
+         }
+         semiIndustrialCollection(limit: 50) {
+           items {
+             name
+             url
+             company
+             temperatureRange
+             isInverter
+             type
+             description
+             imageCollection(limit: 4) {
+               items {
+                 url
+               }
+             }
+             semiCondModelCollection(limit: 10) {
+               items {
+                 price
+                 model
+               }
+             }
+           }
+         }
+       }
+      `);
    return (
       <div className={styles.aircond}>
          <div className="container">
             <NextBreadcrumb homeElement={"Главная"} separator={"/"} />
-            <Grid items={data.data.semiIndustrialCollection.items} currencyVal={data.data.dollarValue.value} title={title} type={type} uri={uri} />
+            <GridContainer items={data.data.semiIndustrialCollection.items} currencyVal={data.data.dollarValue.value} title={title} type={type} uri={uri} />
          </div>
       </div>
    );
