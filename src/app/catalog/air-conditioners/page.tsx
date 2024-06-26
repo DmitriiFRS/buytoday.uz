b9bbc6aa779c929af3af015fe32925995e1a71d7
Дмитрий <Dmitriifrs@gmail.com>
2024-06-26 13:35:36 +0500

    fix

--- src/app/catalog/air-conditioners/page.tsx
+++ src/app/catalog/air-conditioners/page.tsx
@@ -88,7 +88,7 @@ export const metadata = {
    description: "Каталог бытовых кондиционеров в Ташкенте по выгодным ценам",
    keywords: ["кондиционеры", "каталог", "кондиционеры в Ташкенте", "сплит-системы", "бытовые", "air-conditioners", "conditioners"],
 };
-const url = "http://localhost:3000/catalog/air-conditioners";
+const url = "https://buytoday.uz/catalog/air-conditioners";
 
 async function page({ searchParams }: { searchParams: ReadonlyURLSearchParams }) {
    const urlParams = new URLSearchParams(searchParams);
