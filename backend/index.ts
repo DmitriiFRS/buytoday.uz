9581f099b7bde06ef61d56b18c534645d0170321
Дмитрий <Dmitriifrs@gmail.com>
2024-06-29 12:29:13 +0500

    fix routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,8 @@
 import express from "express";
-import { Response } from "express";
 import "dotenv/config";
 import router from "./routes/main";
 const app = express();
 const port = 4001;
-
-app.get("/api", (res: Response) => {
-   res.send(JSON.stringify("API Server"));
-});
 
 app.use(router);
 
--- backend/routes/main.ts
+++ backend/routes/main.ts
@@ -3,6 +3,9 @@ import { getAirconds } from "../controllers/airconds";
 
 const router = express.Router();
 
+router.get("/api", (req, res) => {
+   res.send(JSON.stringify("Buy today API Server"));
+});
 router.get("/api/aircond", getAirconds);
 
 export default router;
