17d5c4acd35b70b249cb97eaa6abc75daf02d788
Dmitriy <dmitriifrs@gmail.com>
2025-01-17 10:46:33 +0500

    upd

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -7,102 +7,102 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
 const socials = [
-   {
-      id: 0,
-      icon: <FaTelegramPlane />,
-      href: "https://t.me/Buytodayuz",
-      className: styles.footer__tg,
-   },
-   {
-      id: 1,
-      icon: <FaFacebookF />,
-      href: "/",
-      className: styles.footer__fb,
-   },
-   {
-      id: 2,
-      icon: <FaInstagram />,
-      href: "/",
-      className: styles.footer__inst,
-   },
+     {
+          id: 0,
+          icon: <FaTelegramPlane />,
+          href: "https://t.me/Buytodayuz",
+          className: styles.footer__tg,
+     },
+     {
+          id: 1,
+          icon: <FaFacebookF />,
+          href: "/",
+          className: styles.footer__fb,
+     },
+     {
+          id: 2,
+          icon: <FaInstagram />,
+          href: "/",
+          className: styles.footer__inst,
+     },
 ];
 
 const info = [
-   {
-      id: 0,
-      title: "Доставка",
-      href: "/delivery",
-   },
-   {
-      id: 1,
-      title: "Сервис",
-      href: "/service",
-   },
-   {
-      id: 2,
-      title: "Акции и скидки",
-      href: "/promotions",
-   },
+     {
+          id: 0,
+          title: "Доставка",
+          href: "/delivery",
+     },
+     {
+          id: 1,
+          title: "Сервис",
+          href: "/service",
+     },
+     {
+          id: 2,
+          title: "Акции и скидки",
+          href: "/promotions",
+     },
 ];
 
 const contacts = [
-   {
-      id: 0,
-      title: "+998 99 797 14 07",
-      href: "+998997971407",
-   },
-   {
-      id: 1,
-      title: "buytoday.uz@gmail.com",
-      href: "buytoday.uz@gmail.com",
-   },
+     {
+          id: 0,
+          title: "+998 77 134 36 60",
+          href: "+998771343660",
+     },
+     {
+          id: 1,
+          title: "buytoday.uz@gmail.com",
+          href: "buytoday.uz@gmail.com",
+     },
 ];
 
 function Footer() {
-   return (
-      <footer className={styles.footer}>
-         <div className={`container ${styles.footer__container}`}>
-            <Link href={"/"} className={styles.footer__logo}>
-               <Image src={logo} alt="Midea" fill style={{ objectFit: "contain" }} />
-            </Link>
-            <div className={styles.footer__social}>
-               <h4 className={styles.footer__title}>Социальные сети</h4>
-               <div className={styles.footer__social__content}>
-                  {socials.map((el) => {
-                     return (
-                        <Link href={el.href} target="_blank" key={el.id} className={`${styles.footer__social__icon} ${el.className}`}>
-                           {el.icon}
-                        </Link>
-                     );
-                  })}
+     return (
+          <footer className={styles.footer}>
+               <div className={`container ${styles.footer__container}`}>
+                    <Link href={"/"} className={styles.footer__logo}>
+                         <Image src={logo} alt="Midea" fill style={{ objectFit: "contain" }} />
+                    </Link>
+                    <div className={styles.footer__social}>
+                         <h4 className={styles.footer__title}>Социальные сети</h4>
+                         <div className={styles.footer__social__content}>
+                              {socials.map((el) => {
+                                   return (
+                                        <Link href={el.href} target="_blank" key={el.id} className={`${styles.footer__social__icon} ${el.className}`}>
+                                             {el.icon}
+                                        </Link>
+                                   );
+                              })}
+                         </div>
+                    </div>
+                    <div className={styles.footer__section}>
+                         <h4 className={styles.footer__title}>Информация</h4>
+                         <div className={styles.footer__content}>
+                              {info.map((el) => {
+                                   return (
+                                        <Link href={el.href} key={el.id} className={styles.footer__content__item}>
+                                             {el.title}
+                                        </Link>
+                                   );
+                              })}
+                         </div>
+                    </div>
+                    <div className={styles.footer__section}>
+                         <h4 className={styles.footer__title}>Контакты</h4>
+                         <div className={styles.footer__content}>
+                              {contacts.map((el, index) => {
+                                   return (
+                                        <Link href={index === 0 ? `tel:${el.href}` : `mailto:${el.href}`} target="_blank" key={el.id} className={styles.footer__content__item}>
+                                             {el.title}
+                                        </Link>
+                                   );
+                              })}
+                         </div>
+                    </div>
                </div>
-            </div>
-            <div className={styles.footer__section}>
-               <h4 className={styles.footer__title}>Информация</h4>
-               <div className={styles.footer__content}>
-                  {info.map((el) => {
-                     return (
-                        <Link href={el.href} key={el.id} className={styles.footer__content__item}>
-                           {el.title}
-                        </Link>
-                     );
-                  })}
-               </div>
-            </div>
-            <div className={styles.footer__section}>
-               <h4 className={styles.footer__title}>Контакты</h4>
-               <div className={styles.footer__content}>
-                  {contacts.map((el, index) => {
-                     return (
-                        <Link href={index === 0 ? `tel:${el.href}` : `mailto:${el.href}`} target="_blank" key={el.id} className={styles.footer__content__item}>
-                           {el.title}
-                        </Link>
-                     );
-                  })}
-               </div>
-            </div>
-         </div>
-      </footer>
-   );
+          </footer>
+     );
 }
 export default Footer;
--- src/Components/Header/HeaderTop.tsx
+++ src/Components/Header/HeaderTop.tsx
@@ -2,14 +2,18 @@ import styles from "./Header.module.scss";
 import Link from "next/link";
 
 function HeaderTop() {
-   return (
-      <div className={styles.header__top}>
-         <div className="container">
-            <Link className={styles.header__top__num} href={"tel:+998997971407"} target="_blank">
-               Для связи: +998 99 797 14 07
-            </Link>
-         </div>
-      </div>
-   );
+     return (
+          <div className={styles.header__top}>
+               <div className="container">
+                    <Link className={styles.header__top__num} href={"tel:+998771343660"} target="_blank">
+                         Для связи: +998 77 134 36 60
+                    </Link>
+                    <span className={styles.header__top__num}> , </span>
+                    <Link className={styles.header__top__num} href={"tel:+998771393660"} target="_blank">
+                         +998 77 139 36 60
+                    </Link>
+               </div>
+          </div>
+     );
 }
 export default HeaderTop;
