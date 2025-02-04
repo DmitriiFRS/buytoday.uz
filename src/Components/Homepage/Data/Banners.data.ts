0706b6e4d69aa45039871e6ad55d870cce93d5d7
Dmitriy <dmitriifrs@gmail.com>
2025-02-04 19:06:24 +0500

    upd

--- src/Components/Homepage/Data/Banners.data.ts
+++ src/Components/Homepage/Data/Banners.data.ts
@@ -1,31 +1,27 @@
-import albapc from "../../../../public/Img/Homepage/Slider/albapc.jpg";
-import albam from "../../../../public/Img/Homepage/Slider/albam.jpg";
-import nordicpc from "../../../../public/Img/Homepage/Slider/nordicpc.jpg";
-import nordicm from "../../../../public/Img/Homepage/Slider/nordicm.jpg";
-import breezpc from "../../../../public/Img/Homepage/Slider/breezpc.jpg";
-import breezm from "../../../../public/Img/Homepage/Slider/breezm.jpg";
-import v8pc from "../../../../public/Img/Homepage/Slider/v8pc.jpg";
-import v8m from "../../../../public/Img/Homepage/Slider/v8m.jpg";
+import naomi from "@/../public/Img/Homepage/Slider/naomi.webp";
+import cas from "@/../public/Img/Homepage/Slider/cas-cond.webp";
+import duct from "@/../public/Img/Homepage/Slider/duct-cond.webp";
+import col from "@/../public/Img/Homepage/Slider/col-cond.webp";
 
 export const bannerImages = [
      {
-          href: "/catalog/air-conditioners/alba-07",
-          pc: albapc,
-          mobile: albam,
+          href: "/catalog/air-conditioners/naomi-07",
+          pc: naomi,
+          mobile: naomi,
      },
      {
-          href: "/catalog/air-conditioners/nordic-09",
-          pc: nordicpc,
-          mobile: nordicm,
+          href: "/catalog/cassette-conditioners",
+          pc: cas,
+          mobile: cas,
      },
      {
-          href: "/catalog/air-conditioners/breezeless-12",
-          pc: breezpc,
-          mobile: breezm,
+          href: "/catalog/duct-conditioners",
+          pc: duct,
+          mobile: duct,
      },
      {
-          href: "/catalog/vrf-outer/vrf-v8-combined_mv8-252wv2gn1-(pro)",
-          pc: v8pc,
-          mobile: v8m,
+          href: "/catalog/col-conditioners",
+          pc: col,
+          mobile: col,
      },
 ];
