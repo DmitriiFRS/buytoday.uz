ebc3af11a5f2f3804fce4daee4147ca294169139
Dmitriy <dmitriifrs@gmail.com>
2025-02-12 18:38:36 +0500

    upd

--- src/ComponentsNew/Homepage/Catalog/CatalogCategories.tsx
+++ src/ComponentsNew/Homepage/Catalog/CatalogCategories.tsx
@@ -1,8 +1,9 @@
-import rac from "@/../public/Img/Catalog/midea-aircond.webp";
-import lcac from "@/../public/Img/Catalog/midea-lcac.webp";
-import cac from "@/../public/Img/Catalog/midea-cac.webp";
-import wha from "@/../public/Img/Catalog/midea-wha.webp";
-import mda from "@/../public/Img/Catalog/midea-mda.webp";
+import rac from "@/../public/Img/Homepage/Catalog/airconds-pc.webp";
+import racM from "@/../public/Img/Homepage/Catalog/airconds-mob.webp";
+import lcac from "@/../public/Img/Homepage/Catalog/lcac.webp";
+import cac from "@/../public/Img/Homepage/Catalog/cac.webp";
+import wha from "@/../public/Img/Homepage/Catalog/wha.webp";
+import mda from "@/../public/Img/Homepage/Catalog/mda.webp";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -13,6 +14,7 @@ const CatalogCategories: React.FC<Props> = ({}) => {
           id: 0,
           title: "Бытовые кондиционеры",
           image: rac,
+          imageMobile: racM,
           slug: "/catalog/air-conditioners",
      };
      const categories = [
@@ -44,23 +46,34 @@ const CatalogCategories: React.FC<Props> = ({}) => {
      return (
           <>
                <div>
-                    <Link href={racCategory.slug}>
+                    <Link href={racCategory.slug} className="group overflow-hidden block rounded-[10px]">
                          <Image
                               src={racCategory.image}
                               alt={racCategory.title}
                               width={1920}
                               height={1080}
-                              className="object-cover rounded-[10px] h-auto w-full aspect-[288/198] lg:aspect-[3.017/1]"
+                              className="object-cover h-auto w-full aspect-[288/198] lg:aspect-[3.017/1] hidden lg:block group-hover:scale-105 duration-700"
                          />
-                         <div className="text-[20px] mt-4 font-medium leading-[110%]">{racCategory.title}</div>
+                         <Image
+                              src={racCategory.imageMobile}
+                              alt={racCategory.title}
+                              width={1920}
+                              height={1080}
+                              className="object-cover h-auto w-full aspect-[288/198] lg:aspect-[3.017/1] block lg:hidden"
+                         />
                     </Link>
                </div>
                <div className="grid grid-cols-1 gap-[35px] lg:grid-cols-2 mt-[35px]">
                     {categories.map((el, index) => {
                          return (
-                              <Link href={el.slug} key={el.id}>
-                                   <Image src={el.image} alt={el.title} width={1920} height={1080} className="object-cover rounded-[10px] h-auto w-full aspect-[288/198]" />
-                                   <div className="text-[20px] mt-4 font-medium leading-[110%]">{el.title}</div>
+                              <Link href={el.slug} key={el.id} className="group overflow-hidden block rounded-[10px]">
+                                   <Image
+                                        src={el.image}
+                                        alt={el.title}
+                                        width={1920}
+                                        height={1080}
+                                        className="object-cover h-auto w-full aspect-[288/198] group-hover:scale-105 duration-700"
+                                   />
                               </Link>
                          );
                     })}
