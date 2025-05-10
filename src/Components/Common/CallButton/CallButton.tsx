eb64eac2a06945c0a5079b60e88335f6e90800de
Дмитрий Фурс <dmitrii@MacBook-Air-Dmitrij.local>
2025-05-10 19:09:49 +0500

    added pulse icon to CallButton component

--- src/Components/Common/CallButton/CallButton.tsx
+++ src/Components/Common/CallButton/CallButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./s.module.scss";
 import Image from "next/image";
 import calltg from "@/../public/Icons/calltg.svg";
@@ -8,32 +8,61 @@ import callButton from "@/../public/Icons/callButton.svg";
 import blackCross from "@/../public/Icons/blackcross.svg";
 
 const CallButton = () => {
-     const [open, setOpen] = useState("none");
+	const [open, setOpen] = useState("none");
+	const [newMessageIcon, setNewMessageIcon] = useState(false);
 
-     const handleChange = () => {
-          if (open === "opened") {
-               setOpen("closed");
-          } else {
-               setOpen("opened");
-          }
-     };
-     return (
-          <div className={classes.button_wrapper}>
-               <div className={`${classes.buttons} ${classes[open]}`}>
-                    <a href={"https://t.me/+998771343660"} target="_blank">
-                         <Image src={calltg} alt="CallButton" width={49} height={49} className={classes.button} />
-                    </a>
-                    <a href={"tel:" + "+998771343660"}>
-                         <Image src={callButton} alt="CallButton" width={49} height={49} className={classes.button} />
-                    </a>
-               </div>
-               {open === "opened" ? (
-                    <Image src={blackCross} alt="CallButton" width={49} height={49} className={classes.button} onClick={handleChange} />
-               ) : (
-                    <Image src={callButton} alt="CallButton" width={49} height={49} className={classes.button} onClick={handleChange} />
-               )}
-          </div>
-     );
+	useEffect(() => {
+		setTimeout(() => {
+			setNewMessageIcon(true);
+		}, 4000);
+	}, []);
+
+	const handleChange = () => {
+		if (open === "opened") {
+			setOpen("closed");
+		} else {
+			setOpen("opened");
+		}
+	};
+	return (
+		<div className={classes.button_wrapper}>
+			<div className={`${classes.buttons} ${classes[open]}`}>
+				<a href={"https://t.me/+998771343660"} target="_blank" className="relative">
+					{newMessageIcon && (
+						<div className="absolute z-[1] top-1 right-1 w-4 h-4">
+							{/* Пульсирующий круг */}
+							<span className="absolute top-0 right-0 inline-flex w-full h-full rounded-full bg-red-400 animate-ping"></span>
+							{/* Фиксированный красный кружок с цифрой */}
+							<div className="relative flex items-center justify-center w-full h-full rounded-full bg-red-500">
+								<span className="text-white text-xs">1</span>
+							</div>
+						</div>
+					)}
+					<Image src={calltg} alt="CallButton" width={49} height={49} className={classes.button} />
+				</a>
+				<a href={"tel:" + "+998771343660"}>
+					<Image src={callButton} alt="CallButton" width={49} height={49} className={classes.button} />
+				</a>
+			</div>
+			{open === "opened" ? (
+				<Image src={blackCross} alt="CallButton" width={49} height={49} className={classes.button} onClick={handleChange} />
+			) : (
+				<button className="relative" onClick={handleChange}>
+					{newMessageIcon && (
+						<div className="absolute z-[1] top-1 right-1 w-4 h-4">
+							{/* Пульсирующий круг */}
+							<span className="absolute top-0 right-0 inline-flex w-full h-full rounded-full bg-red-400 animate-ping"></span>
+							{/* Фиксированный красный кружок с цифрой */}
+							<div className="relative flex items-center justify-center w-full h-full rounded-full bg-red-500">
+								<span className="text-white text-xs">1</span>
+							</div>
+						</div>
+					)}
+					<Image src={callButton} alt="CallButton" width={49} height={49} className={classes.button} />
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default CallButton;
